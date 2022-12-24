c9e364013058b8345463131c869436a47470b939
Rian <rian.azevedo@qintess.com>
2022-12-24 01:05:02 -0300

    componente produto e carrousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,29 @@ import camiseta1 from '../assets/1.png'
 import camiseta2 from '../assets/2.png'
 import camiseta3 from '../assets/3.png'
 
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+
+
 export default function Home() {
-  return (<HomeContainer>
-    <Product>
+  const [sliderRef, instanceRef] = useKeenSlider(
+    {
+      slides: {
+        perView: 3,
+        spacing: 48
+      },
+      slideChanged() {
+        console.log('slide changed')
+      },
+    },
+    [
+      // add plugins here
+    ]
+  )
+
+
+  return (<HomeContainer ref={sliderRef} className="keen-slider">
+    <Product className="keen-slider__slide">
       <Image src={camiseta1} width={520} height={480} alt="" />
       <footer>
         <strong>Camiseta X</strong>
@@ -16,8 +36,15 @@ export default function Home() {
       </footer>
     </Product>
 
-    <Product>
+    <Product className="keen-slider__slide">
       <Image src={camiseta2} width={520} height={480} alt="" />
+      <footer>
+        <strong>Camiseta X2</strong>
+        <span>R$ 189,00</span>
+      </footer>
+    </Product>
+    <Product className="keen-slider__slide">
+      <Image src={camiseta3} width={520} height={480} alt="" />
       <footer>
         <strong>Camiseta X2</strong>
         <span>R$ 189,00</span>
---
+++ src/stitches/pages/home.ts
@@ -0,0 +1,46 @@
+import { styled } from "..";
+
+export const HomeContainer = styled('main', {
+    display: 'flex',
+    width: '100%',
+    maxWidth: 'calc(100vw - ((100vw - 1180px)/2))',
+    minHeight: 656,
+    marginLeft: 'auto'
+})
+
+export const Product = styled('a', {
+    background: `linear-gradient(180deg, #1EA483 0%, #7465D4 100%)`,
+    borderRadius: 8,
+    cursor: "pointer",
+    position: "relative",
+
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+
+    img: {
+        objectFit: "cover"
+    },
+    footer: {
+        position: "absolute",
+        bottom: '0.25rem',
+        right: '0.25rem',
+        left: '0.25rem',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '2rem',
+        borderRadius: 6,
+        backgroundColor: 'rgba(32,32,36,0.9)',
+        strong: {
+            color: '#E1E1E6',
+            fontSize: 20,
+            fontWeight: 'bold'
+        },
+        span: {
+            color: '#00B37E',
+            fontSize: 24,
+            fontWeight: 'bold',
+        },
+
+    }
+})
