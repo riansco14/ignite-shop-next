f08a3d5c4b9ef687ae86df0d86a702323a0023be
Rian <rian.azevedo@qintess.com>
2023-01-09 00:49:49 -0300

    redirect quando for um id inexistente ou invalido

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -34,11 +34,31 @@ export default function Success({ customerName, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    if (!query.session_id) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
     const sessionId = String(query.session_id)
 
-    const session = await stripe.checkout.sessions.retrieve(sessionId, {
-        expand: ['line_items', 'line_items.data.price.product']
-    })
+    let session;
+    try {
+        session = await stripe.checkout.sessions.retrieve(sessionId, {
+            expand: ['line_items', 'line_items.data.price.product']
+        })
+    } catch (error) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+    
 
     const customerName = session.customer_details?.name
     const product = session.line_items?.data[0].price?.product as Stripe.Product
